b660fa5f1c2f3a923f45845bd133b5f5fa6dc814
Scout McFall <scoutmknz@gmail.com>
2021-09-20 23:47:44 +0000

    save first two fxns

--- arrays.js
+++ arrays.js
@@ -3,17 +3,34 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (let i = 0; i < items.length; i += 1) {
+    console.log("${items[i]} ${i}");
+  } 
 }
+// for i in range(len(items)):
+// print(f'{items[i]} {i}')
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+  for (let i = 0; i < items.length; i += 1) {
+    if (i % 2 === 0) {
+      result.push(items[i]);
+    }
+  console.log(result);
+  }
 }
+// result = []
 
+// for i in range(len(items)):
+//     if i % 2 == 0:
+//         result.append(items[i])
+
+// print(result)
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort();
+  let sortedNItems = sortedItems.slice(0, n);
 }

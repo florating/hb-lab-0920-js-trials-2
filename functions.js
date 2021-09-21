4163286d4208a809a97163a9f1fa54e02773cb23
Scout McFall <scoutmknz@gmail.com>
2021-09-21 00:31:45 +0000

    add #1 in object.js

--- functions.js
+++ functions.js
@@ -9,10 +9,40 @@ function isHomeTown(town) {
 
 
 // 2. getFullName
-
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
 // Define your function here
 
 
 // 3. calculateTotal
-
+function calculateTotal(basePrice, state, tax=0.05) {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;
+    } else if (state === 'PA') {
+        fee = 2;
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+    return subtotal + fee;
+}
 // Define your function here
+
+
+function test() {
+    console.log("This is #1:");
+    console.log(isHomeTown('San Francisco'));
+    
+    
+    console.log("This is #2:");
+    console.log(getFullName('Bob', 'The Builder'));
+
+    console.log("This is #3:");
+    console.log(calculateTotal(10, 'CA'));
+}
